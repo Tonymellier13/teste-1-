b7e3e3e12be5530e35663cfaf71cf2a71b4f1eba
59013-76-08 <59013-76-08@exchange.ad.afpanet>
2021-02-08 16:47:07 +0100

    js

---
+++ js/intro.js
@@ -0,0 +1,74 @@
+// const x = 10;
+// console.log(x);
+// let y = 20;
+// console.log(y);
+// y = 41;
+// console.log(y);
+// let z = y % 4;
+// console.log(z);
+// y++;
+// console.log(y);
+// z--;
+// console.log(z);
+// console.log("la valeur de y est :" +y)
+// y+=11;// y=y+11;
+// y-=3// y=y-3;
+// let chaine="chaaine\"mot en cotte\"reste de la chaine";
+// console.log(chaine);
+// chaine='it\'s good day';
+// console.log(chaine);
+// dossier="projet\\images\\image.png";
+// console.log(dossier);
+// chaine="\premiere ligne\ndeuxieme ligne\ntroisieme ligne";
+// console.log(chaine);
+// let person = {
+//     firstName: "tony",
+//     lastName : "mellier",
+//     id       : 1305,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+//   console.log(person.firstName);
+//   console.log(person.lastName);
+//   console.log(person.id);
+//   console.log(person.fullName());
+function afficherMessage() {
+  console.log("un message");
+}
+afficherMessage();
+function some(a, b) {
+  return a + b;
+}
+let x;
+x = some(2, 3);
+console.log(x);
+x = some(6, 12);
+console.log(x);
+function somess(a, b) {
+  console.log(a + b);
+}
+somess(5, 4);
+somess(10, 3);
+
+let val1 = Math.random() * 10;
+console.log(val1);
+let val2 = Math.random() * 10;
+console.log(val2);
+x = some(val1, val2);
+console.log(x);
+s = Math.floor(Math.random() * 10);
+
+console.log(s);
+
+function egale(c, d) {
+  return c == d;
+}
+
+console.log(egale(6, 6));
+
+console.log(egale(9, 9));
+let chaine = " ";
+console.log(Boolean(chaine));
+ chaine = "";
+console.log(Boolean(chaine));
